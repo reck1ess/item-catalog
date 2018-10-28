ff19338c9d45ba9b02e0ca4bf7a6eaa3720c4084
reck1ess <heeinso@snu.ac.kr>
2018-10-28 15:24:54 +0900

    Set up the database

---
+++ database_setup.py
@@ -0,0 +1,56 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    picture = Column(String(300))
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+
+class CategoryItem(Base):
+    __tablename__ = 'category_item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(300))
+    category = relationship(Category)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'category': self.category.name,
+        }
+
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.create_all(engine)
